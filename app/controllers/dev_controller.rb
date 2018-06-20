41c0b41061d6e0772c898a9e584c82815bcd3b6b
Philipp T <philipp@hitchhackers.net>
2018-06-21 00:26:14 +0200

    dev: extracted working copy into partial, updating on response to git pull

--- app/controllers/dev_controller.rb
+++ app/controllers/dev_controller.rb
@@ -5,36 +5,19 @@ class DevController < ApplicationController
   end
 
   def index
-    vop_root = "#{ENV["HOME"]}/projects/virtualop"
-
-    @working_copy_names = %w|vop plugins services web|
-    @working_copies = @working_copy_names.map do |x|
-      hash = {
-        name: x,
-        path: "#{vop_root}/#{x}",
-      }
-      hash[:html_name] = hash[:path].gsub("/", "-")
-      hash
-    end
-
-    @changes = {}
-    @working_copies.each do |working_copy|
-      name = working_copy[:name]
-
-      coordinates = {
-        machine: "localhost", "working_copy" => working_copy[:path]
-      }
-      @changes[name] = $vop.git_status(coordinates)
-      working_copy[:current_revision] = $vop.current_revision(coordinates)
-    end
-
-    #flash.now[:notice] = "beeblebrox!"
+    @working_copies = $vop.working_copies_with_detail(machine: "localhost")
   end
 
   def git_pull
-    result = $vop.git_pull(machine: "localhost", "working_copy" => working_copy_path())
-    last_line = result.lines.last
-    render json: last_line.to_json()
+    revision = $vop.git_pull(machine: "localhost", "working_copy" => working_copy_path())
+    working_copy = {
+      name: params[:working_copy],
+      path: working_copy_path(),
+      html_name: working_copy_path().gsub("/", "-"),
+      current_revision: revision,
+      changes: $vop.git_status(machine: "localhost", "working_copy" => working_copy_path())
+    }
+    render partial: "working_copy", locals: { working_copy: working_copy }, layout: nil
   end
 
   def git_status
