2cdc41848bc5c42ce0d3238346ba0410e55230d0
Philipp T <philipp@hitchhackers.net>
2018-09-16 22:29:41 +0200

    make cable domain configurable

--- config/initializers/vop.rb
+++ config/initializers/vop.rb
@@ -1,1 +1,18 @@
 $vop = Vop.setup
+
+domain_root = ENV.fetch("VOP_DOMAIN") {
+  default_domain = "localhost:3000"
+  $logger.debug "no vop domain configured"
+  nil
+}
+unless domain_root.nil?
+  cable_domain = ENV.fetch("VOP_DOMAIN_CABLE") { "cable.#{domain_root}" }
+
+  $logger.info "vop domain : #{domain_root}"
+  $logger.info "cable domain : #{cable_domain}"
+
+  Rails.application.configure do
+    config.action_cable.url = "ws://#{cable_domain}"
+    config.action_cable.allowed_request_origins = [ /http:\/\/#{domain_root}.*/ ]
+  end
+end
