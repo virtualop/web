0b2231d7ac950eac75ccd8a54a5fe56a5b89a9c4
Philipp T <philipp@hitchhackers.net>
2018-09-21 22:26:08 +0200

    read /etc/vop/web.conf.sh

--- bin/web.sh
+++ bin/web.sh
@@ -3,6 +3,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
+CONFIG_FILE=/etc/vop/web.conf.sh
+
+if [[ -f $CONFIG_FILE ]]; then
+  . $CONFIG_FILE
+fi
+
 bundle exec rails s
 
 cd -
