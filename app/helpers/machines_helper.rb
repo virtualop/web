5d8e098d47caed9c98e8c94c1f051250af2cc6b3
Philipp T <philipp@hitchhackers.net>
2019-04-04 18:46:27 +0200

    reshuffle machine view

--- app/helpers/machines_helper.rb
+++ app/helpers/machines_helper.rb
@@ -1,8 +1,8 @@
 module MachinesHelper
 
-  def render_box(name)
+  def render_box(name, size = 4)
     part = controller.render_to_string partial: name
-    render partial: "machine_box", locals: { name: name, part: part }
+    render partial: "machine_box", locals: { name: name, part: part, size: size }
   end
 
 end
